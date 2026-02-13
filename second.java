0411835ae59f48aa981ba7e12050b1da2877fd38
raghuramsunkara <raghusunkara18@gamil.com>
2026-02-13 08:04:20 +0530

    modified second

--- second.java
+++ second.java
@@ -1,5 +1,5 @@
-public class second {
+public class Second {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("unnbuavdshb ovjj ova p");
     }
 }
